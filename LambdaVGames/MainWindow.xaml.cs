af2ba04e9e9b9f1568aefabc01a28124a7537b45
Blabliblubpaul <86830445+Blabliblubpaul@users.noreply.github.com>
2024-12-03 08:15:13 +0100

    Added MySql.Data Package Added basic database connection for testing

--- LambdaVGames/MainWindow.xaml.cs
+++ LambdaVGames/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace LambdaVGames;
 
@@ -15,7 +8,27 @@ namespace LambdaVGames;
 /// Interaction logic for MainWindow.xaml
 /// </summary>
 public partial class MainWindow : Window {
+    private static MySqlConnection connection;
+    private const string dbConnection = "server=localhost;user id=Test;password=Test;database=LambdaVGamesDb";
+    
     public MainWindow() {
         InitializeComponent();
+
+        using (connection = new MySqlConnection(dbConnection)) {
+            connection.Open();
+
+            if (connection.State == System.Data.ConnectionState.Open) {
+                MessageBox.Show("Connection established.");
+            }
+            else {
+                MessageBox.Show("Unable to connect to database.");
+            }
+        }
+    }
+
+    protected override void OnClosing(CancelEventArgs e) {
+        connection.Close();
+        
+        base.OnClosing(e);
     }
 }
