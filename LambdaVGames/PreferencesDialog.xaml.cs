aa61270c1591877089a996d95d1c6660d7ddd306
FelixAlcor <94474092+FelixAcedia@users.noreply.github.com>
2024-12-11 14:44:51 +0100

    - Added Close functionality to Cancel button

--- LambdaVGames/PreferencesDialog.xaml.cs
+++ LambdaVGames/PreferencesDialog.xaml.cs
@@ -6,4 +6,8 @@ public partial class PreferencesDialog : Window{
     public PreferencesDialog() {
         InitializeComponent();
     }
+
+    private void OnCancleClick(object sender, RoutedEventArgs e) {
+        this.Close();
+    }
 }
