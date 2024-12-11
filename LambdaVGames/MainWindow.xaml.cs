30f03192049b0fdcc90bfd199ab248ab7144f59e
Blabliblubpaul <86830445+Blabliblubpaul@users.noreply.github.com>
2024-12-11 13:51:59 +0100

    Implemented a way to change the database connection.

--- LambdaVGames/MainWindow.xaml.cs
+++ LambdaVGames/MainWindow.xaml.cs
@@ -10,19 +10,19 @@ namespace LambdaVGames;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
+/// </summary>
 public partial class MainWindow : Window {
-    private readonly MySqlConnection connection;
+    private MySqlConnection connection;
     public ObservableCollection<Game> Games { get; } = [];
 
     public MainWindow() {
         InitializeComponent();
-
-
+        
+        DataContext = this;
+        
         DatabaseDialog dbDialog = new();
         dbDialog.ShowDialog();
         connection = MySqlInterop.Connection ?? throw new NullReferenceException("Database connection is null.");
-
-        DataContext = this;
     }
 
     protected override void OnClosing(CancelEventArgs e) {
@@ -99,6 +99,26 @@ public partial class MainWindow : Window {
         }
     }
 
+    private void OnMenuLinkClick(object sender, RoutedEventArgs e) {
+        Process.Start(new ProcessStartInfo {
+            FileName = ((MenuItem)sender).Tag.ToString(),
+            UseShellExecute = true
+        });
+    }
+
+    private void OnExitClick(object sender, RoutedEventArgs e) {
+        this.Close();
+    }
+
+    private async void MenuItem_OnClick(object sender, RoutedEventArgs e) {
+        DatabaseDialog dbDialog = new(MySqlInterop.Server?? "localhost", MySqlInterop.Username?? string.Empty, string.Empty, MySqlInterop.Database?? "myDb");
+        dbDialog.ShowDialog();
+        
+        connection = MySqlInterop.Connection ?? connection;
+
+        await RefreshGamesList();
+    }
+    
     private async Task RefreshGamesList() {
         Games.Clear();
 
@@ -125,16 +145,5 @@ public partial class MainWindow : Window {
 
             Games.Add(game);
         }
-    }
-
-    private void OnMenuLinkClick(object sender, RoutedEventArgs e) {
-        Process.Start(new ProcessStartInfo {
-            FileName = ((MenuItem)sender).Tag.ToString(),
-            UseShellExecute = true
-        });
-    }
-
-    private void OnExitClick(object sender, RoutedEventArgs e) {
-        this.Close();
     }
 }
