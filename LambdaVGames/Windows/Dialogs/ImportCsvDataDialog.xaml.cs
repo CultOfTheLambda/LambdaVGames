96b66b6713c26dd2b85e2acba0917e5c8f6002fb
Paul von Napolski <Paul_Beeblebrox42@gmx.de>
2024-12-20 18:14:41 +0100

    Implemented schema export Implemented data export and import

---
+++ LambdaVGames/Windows/Dialogs/ImportCsvDataDialog.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace LambdaVGames.Windows.Dialogs;
+
+/// <summary>
+/// A dialog window to connect to a database.
+/// </summary>
+public partial class ImportCsvDataDialog : Window {
+    private static readonly char[] separators = [';', ',', '\t', '|', ':', '~', ' ', '^'];
+    
+    public char Separator { get; private set; }
+    public bool IgnoreHeader { get; private set; }
+    
+    public ImportCsvDataDialog() {
+        InitializeComponent();
+    }
+
+    private void ImportButton_OnClick(object sender, RoutedEventArgs e) {
+        Separator = separators[SeparatorComboBox.SelectedIndex];
+        IgnoreHeader = IgnoreHeaderCheckbox.IsChecked?? false;
+        
+        Close();
+    }
+}
